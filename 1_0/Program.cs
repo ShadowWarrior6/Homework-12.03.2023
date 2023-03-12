8974577402377b2c5b22a3d84304ded24eb6d84a
Andranik <andran1401@gmail.com>
2023-03-12 17:15:19 +0400

    задача 10

---
+++ 1_0/Program.cs
@@ -0,0 +1,4 @@
+﻿Console.WriteLine("ввидите трёхзначное число");
+int numbers = Convert.ToInt32(Console.ReadLine());
+string stringNumber = Convert.ToString(numbers);
+Console.WriteLine("И Ответ " + stringNumber[1]);
