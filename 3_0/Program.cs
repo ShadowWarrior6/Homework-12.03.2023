d5bdc094a4d01eda8b00fe7421072d3490530d84
Andranik <andran1401@gmail.com>
2023-03-12 18:43:03 +0400

    задача15

---
+++ 3_0/Program.cs
@@ -0,0 +1,22 @@
+﻿
+
+Console.Write("ввидите цифру обозначающую день недели ");
+int dayNumber = Convert.ToInt32(Console.ReadLine());
+
+void CheckingTheDayOfTheWeek (int dayNumber)
+{
+ if (dayNumber == 6 || dayNumber == 7)
+ {
+    Console.WriteLine("да это выходной");
+ }
+
+ else if (dayNumber < 1 || dayNumber > 7)
+ {
+    Console.WriteLine("ты помоему перепутал");
+ }
+
+ else Console.WriteLine("это не выходной  ");
+
+}
+CheckingTheDayOfTheWeek(dayNumber);
+
