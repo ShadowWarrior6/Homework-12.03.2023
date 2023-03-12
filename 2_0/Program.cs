31b32081d4cc04347fcc2fead50f850856afdaee
Andranik <andran1401@gmail.com>
2023-03-12 17:40:52 +0400

    задача 13

---
+++ 2_0/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("ввидите число");
+int anyNumber = Convert.ToInt32(Console.ReadLine());
+string anyNumberText  = Convert.ToString(anyNumber);
+
+if (anyNumberText.Length > 2)
+{
+    Console.WriteLine ("третье  число "+ anyNumberText[2]);
+}
+else
+{
+    Console.WriteLine(" третьего числа нет");
+}
